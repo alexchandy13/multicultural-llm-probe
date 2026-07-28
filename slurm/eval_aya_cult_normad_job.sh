c232e3dd55f7b7757137fa8dfcbe4376cdefce4c
Alex Chandy <33782377+alexchandy13@users.noreply.github.com>
2026-07-28 15:29:02 -0400

    updated eval to work with rtx3090

--- slurm/eval_aya_cult_normad_job.sh
+++ slurm/eval_aya_cult_normad_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=clip
 #SBATCH --account=clip
 #SBATCH --qos=medium
-#SBATCH --gres=gpu:rtxa6000:1
+#SBATCH --gres=gpu:rtx3090:1
 #SBATCH --time=18:00:00
 #SBATCH --mem=48G
 #SBATCH --cpus-per-task=4
