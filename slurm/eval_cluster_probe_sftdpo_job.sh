4dc6282765a28686c7666a4c3f6e8949f7068722
Alex Chandy <33782377+alexchandy13@users.noreply.github.com>
2026-09-20 10:34:10 -0400

    exclusive node

--- slurm/eval_cluster_probe_sftdpo_job.sh
+++ slurm/eval_cluster_probe_sftdpo_job.sh
@@ -6,6 +6,7 @@
 #SBATCH --gres=gpu:rtxa4000:1
 #SBATCH --time=12:00:00
 #SBATCH --mem=48G
+#SBATCH --exclusive
 #SBATCH --cpus-per-task=4
 #SBATCH --array=0-13
 #SBATCH --output=slurm/eval_cluster_probe_sftdpo.%A_%a.out
