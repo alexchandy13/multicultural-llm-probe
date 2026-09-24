c34787ea490c33a480666796b33ee48d2b0ea958
Alex Chandy <33782377+alexchandy13@users.noreply.github.com>
2026-09-24 00:22:44 -0400

    request 2 gpus

--- slurm/culnig_gemma4_job.sh
+++ slurm/culnig_gemma4_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=clip
 #SBATCH --account=clip
 #SBATCH --qos=high
-#SBATCH --gres=gpu:rtxa6000:1
+#SBATCH --gres=gpu:rtxa6000:2
 #SBATCH --time=24:00:00
 #SBATCH --mem=128G
 #SBATCH --cpus-per-task=4
